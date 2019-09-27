ba7e1ce736366f3eb2a19e59a4e2022edffda41c
Diego Castillo <raigiku@hotmail.com>
2019-09-27 01:41:36 -0500

    Adición de medición de memoria y velocidad para la solución óptima

--- Assets/Scripts/Models/Optimal/Main.cs
+++ Assets/Scripts/Models/Optimal/Main.cs
@@ -1,4 +1,7 @@
-﻿namespace Complejidad.Models.Optimal
+﻿using System;
+using System.Diagnostics;
+
+namespace Complejidad.Models.Optimal
 {
     public class Main : Complejidad.Models.Algorithm
     {
@@ -9,7 +12,20 @@
             var width = int.Parse(dimensions[0]);
             var height = int.Parse(dimensions[1]);
             int n = int.Parse(input_lines[1]);
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var initialMemory = GC.GetTotalMemory(false);
+
             Matriz m = new Matriz(width, height);
+
+            var finalMemory = GC.GetTotalMemory(false);
+            stopWatch.Stop();
+            TimeSpan timeElapsedSpan = stopWatch.Elapsed;
+
+            TimeElapsed = timeElapsedSpan.TotalSeconds.ToString();
+            MemoryUsed = ((finalMemory - initialMemory) / 1_024).ToString();
+
             m.printInput();
             m.printResp();
         }
